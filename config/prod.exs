9725cb3b8b78545bd0f5ba3ba68725b804a86823
Cody Clair <cody.clair@pioneerbible.org>
2024-08-21 00:20:27 -0500

    bruh

--- config/prod.exs
+++ config/prod.exs
@@ -17,5 +17,9 @@ config :swoosh, local: false
 # Do not print debug messages in production
 config :logger, level: :info
 
+config :csc_worship, CscWorship.Repo,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
 # Runtime production configuration, including reading
 # of environment variables, is done on config/runtime.exs.
