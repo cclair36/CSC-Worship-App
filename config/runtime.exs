a9ab974561b564c442a4a7b4d003d6aca39ee56b
Cody Clair <cody.clair@pioneerbible.org>
2024-08-24 11:00:27 -0500

    adding cacert to runtime.exs

--- config/runtime.exs
+++ config/runtime.exs
@@ -31,7 +31,7 @@ if config_env() == :prod do
   maybe_ipv6 = if System.get_env("ECTO_IPV6") in ~w(true 1), do: [:inet6], else: []
 
   config :csc_worship, CscWorship.Repo,
-    ssl: true,
+    ssl: [cacertfile: Path.expand("priv/certs/ca.crt")],
     url: database_url,
     pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
     socket_options: maybe_ipv6
