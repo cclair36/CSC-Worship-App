cf0b7f0a0b7ade60d2821eaeab0298cf7d2852be
Cody Clair <cody.clair@pioneerbible.org>
2024-08-27 23:32:07 -0500

    add api key to mailer

--- lib/csc_worship/mailer.ex
+++ lib/csc_worship/mailer.ex
@@ -1,3 +1,3 @@
 defmodule CscWorship.Mailer do
-  use Swoosh.Mailer, otp_app: :csc_worship
+  use Swoosh.Mailer, api_key: "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", otp_app: :csc_worship
 end
