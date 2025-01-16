9ef2abf16dd9c89521450a7dc5e9486a62682a5d
Cody Clair <cody.clair@pioneerbible.org>
2025-01-16 13:36:11 -0600

    well.. i can't hash a password..

--- lib/csc_worship_web/controllers/user_registration_controller.ex
+++ lib/csc_worship_web/controllers/user_registration_controller.ex
@@ -6,9 +6,11 @@ defmodule CscWorshipWeb.UserRegistrationController do
   alias CscWorshipWeb.UserAuth
 
   def new(conn, _params) do
-    conn
-  |> put_flash(:error, "This is prohibited. If you need an account made, contact Cody.")
-  |> redirect(to: "/users/log_in") # "users/log_in")
+    changeset = Accounts.change_user_registration(%User{})
+    render(conn, :new, changeset: changeset)
+    #conn
+    #|> put_flash(:error, "This page is prohibited. Please contact Cody if you need an account made.")
+    #|> redirect(to:"/users/log_in")
   end
 
   def create(conn, %{"user" => user_params}) do
